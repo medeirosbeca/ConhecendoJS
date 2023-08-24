9459a7f7cbbf0ff5132abaa0a5142d42ff174009
medeirosbeca <142314420+medeirosbeca@users.noreply.github.com>
2023-08-24 14:06:43 +0000

    lista enum

---
+++ listaEnum.js
@@ -0,0 +1,14 @@
+/* 1. Suponha que você está desenvolvendo um aplicativo de agendamento e precisa representar os dias
+da semana. Como você usaria enums para isso e qual seria o benefício?*/
+
+/* 2. Você está construindo um jogo e precisa representar os diferentes tipos de inimigos. Como você 
+usaria enums para essa situação?*/
+
+/* 3. Você está criando um aplicativo de reserva de passagens aéreas e precisa representar os
+diferentes tipos de classe (Econômica, Executiva, Primeira Classe). Como você usaria enums para isso?*/
+
+/* 4. Imagine que você está desenvolvendo um aplicativo de tarefas e deseja categorizar as tarefas em
+"Trabalho", "Estudo" e "Lazer". Como você usaria enums para esse cenário?*/
+
+/* 5. Em um aplicativo de e-commerce, você precisa definir os diferentes status de um pedido: "Em
+processamento", "Enviado" e "Entregue". Como você usaria enums para isso?*/
