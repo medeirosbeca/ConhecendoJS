d28691a2ec8d1116d47293ad388e2ec2181a815c
medeirosbeca <142314420+medeirosbeca@users.noreply.github.com>
2023-08-24 12:31:55 +0000

    listaobjeto

---
+++ listaObjeto.js
@@ -0,0 +1,59 @@
+/*1. Como você cria um objeto vazio chamado pessoa em JavaScript?*/
+// const pessoa = {
+//     nome: ""
+// }
+
+/*2. Como você adiciona a propriedade "nome" com o valor "João" a um objeto chamado "pessoa"?*/
+const pessoa = {
+    nome: "João",
+}
+
+console.log(pessoa.nome);
+
+/*3. Como você acessa o valor da propriedade "idade" de um objeto chamado "usuario"?*/
+const usuario ={
+    idade: 33,
+}
+console.log(usuario.idade);
+
+/*4. Suponha que você tenha um objeto "produto" com a propriedade "preco" definida como 50. Como
+você altera o valor dessa propriedade para 75?*/
+const produto ={
+    preco: 50,
+}
+const produto1 = new Object();
+produto1.valor = 75;
+
+console.log(produto1.valor);
+
+/*5. Como você remove a propriedade "email" de um objeto chamado "contato"?*/
+const contato ={
+    email1: "rm7765@gmail.com",
+    email2: "wagneh@gmail.com"
+}
+delete contato.email2
+
+console.log(contato.email1);
+
+/*6. Como você adiciona uma propriedade "endereco" com o valor "Rua A, 123" a um objeto existente
+chamado "cliente"?*/
+
+
+
+/*7. Se você tem um objeto "empresa" com uma propriedade "departamentos", como você acessa o nome
+do primeiro departamento?*/
+
+
+
+/*8. Suponha que você tenha um objeto "aluno" com a propriedade "notas" contendo [85, 90, 78]. Como
+você altera a segunda nota para 95?*/
+
+
+
+/*9. Se você tem um objeto "dados" com uma propriedade "valores" contendo [10, 20, 30], como você 
+remove o segundo valor (20) desse array?*/
+
+
+
+/*10. Como você cria um objeto com as propriedades "titulo" (com o valor "Livro") e "autor" (com o
+valor "Maria")?*/
